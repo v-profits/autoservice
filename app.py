1a86e708ac1f6797d448077be46c98a35523a13a
v-profits <v-profits@mail.ru>
2025-05-20 14:38:33 +0400

    Fix service add error when date is missing

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from models import db
+from models import db, User  # добавили импорт User
 from routes import create_routes
 
 app = Flask(__name__)
-
-app.secret_key = 'автосервис73'  #  ключ
+app.secret_key = 'автосервис73'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,11 +17,9 @@ create_routes(app)
 @app.before_first_request
 def create_user():
     if not User.query.first():
-        db.create_all()
         admin = User(username='admin', password='admin')
         db.session.add(admin)
         db.session.commit()
-
 
 @app.route('/')
 def index():
@@ -33,5 +30,44 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000, debug=True)
     app.run(host='0.0.0.0', port=5000)
+
+
+
+# from flask import Flask, render_template
+# from models import db
+# from routes import create_routes
+#
+# app = Flask(__name__)
+#
+# app.secret_key = 'автосервис73'  #  ключ
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
+#
+# with app.app_context():
+#     db.create_all()
+#
+# create_routes(app)
+#
+# @app.before_first_request
+# def create_user():
+#     if not User.query.first():
+#         db.create_all()
+#         admin = User(username='admin', password='admin')
+#         db.session.add(admin)
+#         db.session.commit()
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('base.html')
+#
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+#
+# if __name__ == '__main__':
+#     # app.run(host='0.0.0.0', port=5000, debug=True)
+#     app.run(host='0.0.0.0', port=5000)
